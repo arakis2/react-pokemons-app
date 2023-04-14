fb014d8cf2887bc96a6e877f8a57ab1072c4d888
arakis2 <51448379+arakis2@users.noreply.github.com>
2023-04-14 18:05:18 +0200

    first commit

---
+++ src/pages/pokemon-list.tsx
@@ -0,0 +1,28 @@
+import React, { FunctionComponent, useState, useEffect } from 'react';
+import Pokemon from '../models/pokemon';
+import POKEMONS from '../models/mock-pokemon';
+import PokemonCard from '../components/pokemon-card';
+  
+const PokemonList: FunctionComponent = () => {
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  
+  useEffect(() => {
+    setPokemons(POKEMONS);
+  }, []);
+  
+  return (
+    <div>
+      <h1 className="center">Pokédex</h1>
+      <p className='center'>Il y a <b>{pokemons.length}</b> pokémon{pokemons.length > 1 ? 's' : ''} dans le pokédex</p>
+      <div className="container"> 
+        <div className="row"> 
+        {pokemons.map(pokemon => (
+          <PokemonCard key={pokemon.id} pokemon={pokemon}/>
+        ))}
+        </div>
+      </div>
+    </div> 
+  );
+}
+  
+export default PokemonList;
